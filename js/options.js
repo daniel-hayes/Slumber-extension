9c9fcd5f32261501972acb2626c810ee08f550b9
dhayes02 <dhayes02@syr.edu>
2014-06-17 11:12:06 -0400

    fixed options

--- js/options.js
+++ js/options.js
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-// Saves options to chrome.storage
-function save_options() {
-  var color = document.getElementById('color').value;
-  var likesColor = document.getElementById('like').checked;
-  chrome.storage.sync.set({
-    favoriteColor: color,
-    likesColor: likesColor
-  }, function() {
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 750);
-  });
-}
-
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
-function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
-  chrome.storage.sync.get({
-    favoriteColor: 'red',
-    likesColor: true
-  }, function(items) {
-    document.getElementById('color').value = items.favoriteColor;
-    document.getElementById('like').checked = items.likesColor;
-  });
-}
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
-    save_options);
-=======
 $(document).ready(function() {
 
 
@@ -207,6 +173,3 @@ restore_options();
 $('#save').on('click', save_options) // call save options
 */
 });
-
-
->>>>>>> 66ba82f8e7489ba1109222fd887d6914e82bb11b
